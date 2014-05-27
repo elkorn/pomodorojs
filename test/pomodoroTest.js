819bf4c74f4b55cb8dca216040180906844c7afb
Korneliusz Caputa <helluinster@gmail.com>
2014-05-27 14:18:22 +0200

    [PomodoroJS] I'm not gonna embarrass myself by pretending there are any tests here... ;P

--- test/pomodoroTest.js
+++
@@ -1,8 +0,0 @@
-var should = require("should");
-
-describe("Pomodoro Tracker", function() {
-    it("SHOULD exist", function() {
-        var tracker = require("../src/tracker");
-        should.exist(tracker);
-    });
-});
