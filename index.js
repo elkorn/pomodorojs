672f23ad48ba957ea995a7e6e33a4e1404ed3ac9
Korneliusz Caputa <helluinster@gmail.com>
2014-05-27 14:13:55 +0200

    [PomodorJS] Adding sound.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+#!/usr/bin/node
+
 var PomodoroJS = require("./src/pomodoro.js");
 var state = require("./src/state");
 var pr = require("./src/purple-remote");
+var notifier = require("./src/notifier");
+var format = require("util").format;
+var sound = require("./src/sound");
 
+state.resetTime();
 var t = new PomodoroJS();
 
 t.on("pomodoroTick", function(data) {
@@ -9,13 +15,34 @@ t.on("pomodoroTick", function(data) {
 });
 
 t.on("pomodoroStart", function() {
-    console.log("pomodoroStarted");
-
+    notifier.notify("Get to work!");
+    pr.changeStatus({
+        status: "unavailable",
+        message: "Pomodoro"
+    });
+    sound.play();
 });
 
 t.on("pomodoroFinish", function() {
-    console.log("pomodoroFinish");
     state.recordPomodoro();
+    notifier.notify(
+        format(
+            "Finished! Have a %sbreak!",
+            t.currentState() === "bigBreak" ? "long " : ""));
+    pr.changeStatus({
+        status: "available",
+        message: ""
+    });
+    sound.play();
 });
 
 t.start();
+
+process.on("SIGINT", function() {
+    pr.changeStatus({
+        status: "available",
+        message: ""
+    });
+    state.resetTime();
+    process.exit(0);
+});
